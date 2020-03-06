ec003e0277f373d1c88fd157336283667f8b317e
FatimaYari <61829146+FatimaYari@users.noreply.github.com>
2020-03-06 21:21:58 +0100

    Update Pasient.java

--- Pasient.java
+++ Pasient.java
@@ -1,2 +1,27 @@
-# oblig - 4
-class Pasient;
+class Pasient{
+  protected String navn;
+  protected String fodselsnummer;
+  protected int pasientId;
+  protected static int teller = 0;
+  protected Stabel<Resept> resepter;
+
+  public Pasient(String navn, String fodselsnummer){
+    this.navn = navn;
+    this.fodselsnummer = fodselsnummer;
+    pasientId = teller;//unikt id
+    teller ++;
+    resepter = new Stabel<Resept>();
+  }
+  public void leggTilResept(Resept resept){
+    resepter.leggPaa(resept);
+  }
+
+  public Stabel<Resept> hentUtResepter(){
+    return resepter;
+  }
+
+  @Override
+  public String toString(){
+    return "Pasient informasjon" + "\n" + "Navn: " + navn + "\n" + "Fodselsnummer: " + fodselsnummer;
+  }
+}
