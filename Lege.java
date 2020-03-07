edf001701b425c5556538237fa6a3ca2f7648f65
thaksha8 <61829167+thaksha8@users.noreply.github.com>
2020-03-07 16:28:02 +0100

    Update Lege.java

--- Lege.java
+++ Lege.java
@@ -1,21 +1,34 @@
-/*
-Dette er et program som lager en klasse "Lege" for representasjon av leger.
-*/
+/*Dette er et program som lager en klasse "Lege" for representasjon av leger.*/
 
-class Lege {
+class Lege implements Comparable<Lege> {
+  protected Lenkeliste<Resept> utskrevendeResepter;
   protected String navn;
 
-  // @param navn er navnet på legen. 
+  // @param navn er navnet på legen.
+
   public Lege(String navn) {
     this.navn = navn;
   }
 
+
+
   public String hentNavn() {
     return navn;
+  }
+
+//Leger sorteres alfabetisk etter navn.
+  public int compareTo(Lege annen){
+    return navn.compareTo(annen.hentNavn());
+  }
+
+//Metoden gjør at det er mulig å hente ut en liste av resepter, som legen har skrevet.
+  public Lenkeliste<Resept> hentUtResepter(){
+    return utskrevendeResepter;
   }
 
   @Override
   public String toString() {
     return "Lege\n" + "Navn: " + navn;
   }
+
 }
