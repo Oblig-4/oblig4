bedb5df0159a1713ba9439c0bdfce997ab656b3c
FatimaYari <61829146+FatimaYari@users.noreply.github.com>
2020-03-09 13:13:21 +0100

    Update PResept.java

--- PResept.java
+++ PResept.java
@@ -8,8 +8,8 @@ class PResept extends HvitResept {
   // "PResept" arver egenskaper fra "HvitResept", som igjen arver fra "Resept" (uten tilleggsegenskaper),
   // kaller derfor på superklasssens konstruktør vha. super (med de aktuelle parametrene).
   // Reit tas ikke inn i konstruktøren til PResept, men vil konstant være lik 3 for P-resepter. Derfor sender vi inn 3 som verdi for reit inn i superklassens konstruktør.
-  public PResept(Legemiddel legemiddel, Lege utskrivendeLege, int pasientId) {
-    super(legemiddel, utskrivendeLege, pasientId, 3);
+  public PResept(Legemiddel legemiddel, Lege utskrivendeLege, Pasient pasient) {
+    super(legemiddel, utskrivendeLege, pasient, 3);
   }
 
   // Returnerer prisen pasienten må betale. Pasienten betaler 108 kroner mindre for legemiddelet med P-resept.
@@ -23,12 +23,5 @@ class PResept extends HvitResept {
     }
     return nyPris;
   }
-
-  // Overskriver toString() metoden til å returnere all tilgjengelig informasjon om objektet.
-  @Override
-  public String toString() {
-    return "P-resept\n" + "Resept id: " + id + "\nLegemiddel type: " + legemiddel + "\nLege: " + utskrivendeLege.hentNavn() +
-          "\nPasient Id: " + pasientId + "\nReit: " + reit + "\nFarge: " + this.farge() + "\nPris aa betale: " + this.prisAaBetale();
-  }
-
+  
 }
