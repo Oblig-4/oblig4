7133afbe591abb56f9d066b5cd41a8d6be0d6e12
FatimaYari <61829146+FatimaYari@users.noreply.github.com>
2020-03-09 13:10:44 +0100

    Update BlaaResept.java

--- BlaaResept.java
+++ BlaaResept.java
@@ -6,7 +6,7 @@ for representasjon av blå resepter.
 class BlaaResept extends Resept {
 
   // "BlaaResept" arver egenskaper fra "Resept" (uten tilleggsegenskaper) , kaller derfor på superklasssens konstruktør vha. super (med de aktuelle parametrene).
-  public BlaaResept(Legemiddel legemiddel, Lege utskrivendeLege, int pasientId, int reit) {
+  public BlaaResept(Legemiddel legemiddel, Lege utskrivendeLege, Pasient pasient, int reit) {
     super(legemiddel, utskrivendeLege, pasientId, reit);
   }
 
