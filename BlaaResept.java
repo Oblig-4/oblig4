63a481a34cfc12b0f522a6d88f97fe122f1ee872
thaksha8 <61829167+thaksha8@users.noreply.github.com>
2020-03-10 15:57:04 +0100

    Update BlaaResept.java

--- BlaaResept.java
+++ BlaaResept.java
@@ -1,32 +1,62 @@
 /*
+
 Dette er et program som lager en subklasse "BlaaResept" av superklassen "Resept",
+
 for representasjon av blå resepter.
+
 */
+
+
 
 class BlaaResept extends Resept {
 
+
+
   // "BlaaResept" arver egenskaper fra "Resept" (uten tilleggsegenskaper) , kaller derfor på superklasssens konstruktør vha. super (med de aktuelle parametrene).
+
   public BlaaResept(Legemiddel legemiddel, Lege utskrivendeLege, Pasient pasient, int reit) {
+
     super(legemiddel, utskrivendeLege, pasient, reit);
+
   }
 
+
+
   @Override
+
   public String farge() {
+
     return "blaa";
+
   }
+
+
 
   // Returnerer prisen pasienten må betale, som er 25% av prisen på legemiddelet for blå resept.
+
   @Override
+
   public double prisAaBetale() {
+
     double pris = legemiddel.hentPris();
+
     return pris*0.25;
+
   }
 
+
+
   // Overskriver toString() metoden til å returnere all tilgjengelig informasjon om objektet.
+
   @Override
+
   public String toString() {
-    return "Blaa resept\n" + "Resept id: " + id + "\nLegemiddel type: " + legemiddel + "\nLege: " + utskrivendeLege.hentNavn() + "\nPasient info: " + "\n" + pasient  +
-           "\nReit: " + reit + "\nFarge: " + this.farge() + "\nPris aa betale: " + this.prisAaBetale();
+
+    return "Blaa resept " + "\n" + "Resept id: "  + id + "\n" + "Legemiddel type: " + legemiddel + "\nLege: " + 
+    utskrivendeLege.hentNavn() + "\nPasient info: " + "\n" + pasient + "\n" + "Reit: " + reit + "\n" + "Farge: " + this.farge() + "\n" + "Pris aa betale:" + this.prisAaBetale();
+
   }
+
+
 
 }
