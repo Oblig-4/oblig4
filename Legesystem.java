8515909de69a10c8a57c221d679336c9dfa6795f
FatimaYari <61829146+FatimaYari@users.noreply.github.com>
2020-03-12 12:25:44 +0100

    Update Legesystem.java

--- Legesystem.java
+++ Legesystem.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import java.util.NoSuchElementException;
 
 class Legesystem{
   private Lenkeliste<Pasient> pasienter;
@@ -18,128 +18,152 @@ class Legesystem{
   }
 
 
-  public void lesFraFil(File filnavn) throws FileNotFoundException, UlovligUtskrift{
-
-    Scanner fil;
-    try{
+  public void lesFraFil(File filnavn) throws FileNotFoundException, UlovligUtskrift, NumberFormatException, NoSuchElementException{
+      Scanner fil;
+      try{
         fil = new Scanner(filnavn);
-    }
-    catch (FileNotFoundException e) {
-      throw new FileNotFoundException();
-    }
+      }
+      catch (FileNotFoundException e) {
+        throw new FileNotFoundException();
+      }
 
       String linje = "";
 
-//lager liste med pasienter
+  //lager liste med pasienter
       while (fil.hasNextLine()){
         linje = fil.nextLine();
 
         while (linje.charAt(0) != '#') { //charAt(int index) returnerer char verdi på den gitt indexen.
           linje = fil.nextLine();
-          String[] data = linje.split(",");
-          String p = data[0];
-          String fodselsnummer = data[1];
-          Pasient pasient = new Pasient(p, fodselsnummer);
-          pasienter.leggTil(pasient);
+          try{
+            String[] data = linje.split(",");
+            String p = data[0];
+            String fodselsnummer = data[1];
+            Pasient pasient = new Pasient(p, fodselsnummer);
+            pasienter.leggTil(pasient);
+          }
+          catch(NumberFormatException e){
+            linje = fil.nextLine();
+          }
         }
 
-//lager liste med legemidler
+  //lager liste med legemidler
         while (linje.charAt(0) != '#') {
           linje = fil.nextLine();
-          String[] data = linje.split(",");
-          String navn = data[0];
-          String type = data[1];
-          double pris = Double.parseDouble(data[2]);
-          double virkestoff = Double.parseDouble(data[3]);
-          int styrke = 0;
-          if (data.length == 5){
-            styrke = Integer.parseInt(data[4]);
-          }
+          try{
+            String[] data = linje.split(",");
+            String navn = data[0];
+            String type = data[1];
+            double pris = Double.parseDouble(data[2]);
+            double virkestoff = Double.parseDouble(data[3]);
+            int styrke = 0;
+            if (data.length == 5){
+              styrke = Integer.parseInt(data[4]);
+            }
 
-          if (type == "vanlig") {
-            Vanlig vanlig = new Vanlig(navn, pris, virkestoff);
-            legemidler.leggTil(vanlig);
+            if (type == "vanlig") {
+              Vanlig vanlig = new Vanlig(navn, pris, virkestoff);
+              legemidler.leggTil(vanlig);
+            }
+            if (type == "narkotisk") { //lower
+              Narkotisk narko = new Narkotisk(navn, pris, virkestoff, styrke);
+              legemidler.leggTil(narko);
+            }
+            else if (type == "vanedannende"){
+              Vanedannende vane = new Vanedannende(navn, pris, virkestoff, styrke);
+              legemidler.leggTil(vane);
+            }
           }
-          if (type == "narkotisk") { //lower
-            Narkotisk narko = new Narkotisk(navn, pris, virkestoff, styrke);
-            legemidler.leggTil(narko);
-          }
-          else if (type == "vanedannende"){
-            Vanedannende vane = new Vanedannende(navn, pris, virkestoff, styrke);
-            legemidler.leggTil(vane);
+          catch(NumberFormatException f){
+            linje = fil.nextLine();
           }
         }
 
-//lager liste med leger -sortert
+  //lager liste med leger -sortert
         while (linje.charAt(0) != '#'){
-          linje = fil.nextLine();
-          String[] data = linje.split(",");
-          String navn = data[0];
-          int id = Integer.parseInt(data[1]);
-          if (id == 0){
-            Lege l = new Lege(navn);
-            leger.leggTil(l);
+            linje = fil.nextLine();
+          try{
+            String[] data = linje.split(",");
+            String navn = data[0];
+            int id = Integer.parseInt(data[1]);
+            if (id == 0){
+              Lege l = new Lege(navn);
+              leger.leggTil(l);
+            }
+            Spesialist s = new Spesialist(navn, id);
+            leger.leggTil(s);
+            }
+          catch(NumberFormatException f){
+            linje = fil.nextLine();
           }
-          Spesialist s = new Spesialist(navn, id);
-          leger.leggTil(s);
         }
 
-//lager liste med resepter
+  //lager liste med resepter
         while (linje.charAt(0) != '#'){
           linje = fil.nextLine();
-          String[] data = linje.split(",");
-          int id = Integer.parseInt(data[0]);
-          String navn = data[1];
-          int pId = Integer.parseInt(data[2]);
-          String farge = data[3];
-          int reit = 0;
-          if (data.length == 5){
-            reit = Integer.parseInt(data[4]);
-          }
+          try{
+            String[] data = linje.split(",");
+            int id = Integer.parseInt(data[0]);
+            String navn = data[1];
+            int pId = Integer.parseInt(data[2]);
+            String farge = data[3];
+            int reit = 0;
+            if (data.length == 5){
+              reit = Integer.parseInt(data[4]);
+            }
+            Lege leg = null;
+            Pasient pas = null;
+            Legemiddel lm = null;
 
-          Lege leg = null;
-          Pasient pas = null;
-          Legemiddel lm = null;
+            if(pId < pasienter.stoerrelse() && id < legemidler.stoerrelse()){
+              //finner riktig lege
+              for (int i=0; i < leger.stoerrelse(); i++) {
+                if(leger.hent(i).hentLegensNavn().equals(navn)) {
+                  leg = leger.hent(i);
+                 }
+               }
 
-          //finner riktig lege
-          for (Lege l : leger) {
-            if(l.hentNavn() == navn) {
-              leg = l;
-             }
+              //finner riktig pasient
+              for (Pasient p : pasienter) {
+                if(p.hentPasientId() == pId){
+                  pas = p;
+                }
+              }
+
+              //finner riktig legemiddel
+              for (Legemiddel legem : legemidler) {
+                if(legem.hentId() == id){
+                  lm = legem;
+                }
+              }
+              if (farge == "p"){
+                Resept r = leg.skrivPResept(lm, pas);
+                resepter.leggTil(r);
+              }
+              else if (farge == "hvit") {
+                Resept r = leg.skrivHvitResept(lm, pas, reit);
+                resepter.leggTil(r);
+              }
+              else if (farge == "millitaer"){
+                Resept r = leg.skrivMillitaerResept(lm, pas, reit);
+                resepter.leggTil(r);
+              }
+              else if (farge == "blaa") {
+                Resept r = leg.skrivBlaaResept(lm, pas, reit);
+                resepter.leggTil(r);
+              }
+            }// slutt på if
+            try{
+             linje = fil.nextLine();
            }
-
-           //finner riktig pasient
-          for (Pasient p : pasienter) {
-            if(p.hentPasientId() == pId){
-              pas = p;
-            }
+           catch(NoSuchElementException f){
+             break;
           }
-
-          //finner riktig legemiddel
-          for (Legemiddel legem : legemidler) {
-            if(legem.hentId() == id){
-              lm = legem;
-            }
-          }
-
-          if (farge == "p"){
-            Resept r = leg.skrivPResept(lm, pas);
-            resepter.leggTil(r);
-          }
-
-          else if (farge == "hvit") {
-            Resept r = leg.skrivHvitResept(lm, pas, reit);
-            resepter.leggTil(r);
-          }
-          else if (farge == "millitaer"){
-            Resept r = leg.skrivMillitaerResept(lm, pas, reit);
-            resepter.leggTil(r);
-          }
-          else if (farge == "blaa") {
-            Resept r = leg.skrivBlaaResept(lm, pas, reit);
-            resepter.leggTil(r);
-          }
+        }//slutt på try
+        catch(NumberFormatException f){
+          linje = fil.nextLine();
         }
       }
-   }
+    }
+  }
 }
